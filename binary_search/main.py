05da1307f8658f4128c62355b51c44042d734ad4
santosh <santoshgairesharma@Gaires-MacBook-Air.local>
2026-07-06 06:46:54 +0900

    This is binary search

---
+++ binary_search/main.py
@@ -0,0 +1,39 @@
+# Searching Algorithms
+
+
+ #704. Binary Search
+#Easy
+#Topics
+#premium lock icon
+#Companies
+#Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+#You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+#Example 1:
+
+#Input: nums = [-1,0,3,5,9,12], target = 9
+#Output: 4
+#Explanation: 9 exists in nums and its index is 4
+#Example 2:
+
+#Input: nums = [-1,0,3,5,9,12], target = 2
+#Output: -1
+#Explanation: 2 does not exist in nums so return -1
+
+class solution:
+    def binary_search(self, nums: list[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
+if __name__ == "__main__":
+    a = solution()
+    print(a.binary_search([-1,0,3,5,9,12], 9))
+    print(a.binary_search([-1,0,3,5,9,12], 2))
+
+# But here is one thing to note
+# This is not the best way to solve this searching algorithm
+
